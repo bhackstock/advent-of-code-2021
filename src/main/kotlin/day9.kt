9b2d324ced7a1d441b614b68b21337d1358c5c54
BenniLiveVoice <94900474+BenniLiveVoice@users.noreply.github.com>
2021-12-15 16:57:06 +0100

    Update day9.kt

--- src/main/kotlin/day9.kt
+++ src/main/kotlin/day9.kt
@@ -13,26 +13,66 @@ fun main() {
         }
     }
 
+    val basins = mutableListOf<Int>()
+
     //find low-points
     val lowPoints = mutableListOf<Int>()
     for (i in 1..rows) {
         for (j in 1..columns) {
             val min = minOf(matrix[i + 1][j], matrix[i - 1][j], matrix[i][j + 1], matrix[i][j - 1])
             if (matrix[i][j] < min) {
-                //found local minima
+                //found local minimum
+                val visitedLocations = mutableSetOf<Pair<Int, Int>>()
+                val size = matrix.countNumInBasin(row = i, column = j, visitedLocations)
+                basins.add(size)
                 lowPoints.add(matrix[i][j])
+                println("basin complete, new one next")
             }
         }
     }
 
-    println(lowPoints.sumOf { it + 1 })
+    println("result1 = ${lowPoints.sumOf { it + 1 }}")
+    val result2 = basins.asSequence().sortedDescending().take(3).reduce { a, b -> a * b }
+    println("result2: $result2")
+}
 
-//    for (i in 0 until rows) {
-//        for (j in 0 until columns) {
-//            matrix[i][j] = lines[i][j].digitToInt()
-//        }
-//    }
+private fun Array<Array<Int>>.countNumInBasin(
+    row: Int,
+    column: Int,
+    visitedLocations: MutableSet<Pair<Int, Int>>
+): Int {
+    visitedLocations.add(Pair(row, column))
+    if (this[row][column] == 9) return 0
 
+    val numAbove = if (visitedLocations.contains(Pair(row - 1, column))) {
+        0
+    } else {
+        visitedLocations.add(Pair(row - 1, column))
+        this.countNumInBasin(row - 1, column, visitedLocations)
+    }
 
-    println()
+    val numBelow = if (visitedLocations.contains(Pair(row + 1, column))) {
+        0
+    } else {
+        visitedLocations.add(Pair(row + 1, column))
+        this.countNumInBasin(row + 1, column, visitedLocations)
+    }
+
+    val numLeft = if (visitedLocations.contains(Pair(row, column - 1))) {
+        0
+    } else {
+        visitedLocations.add(Pair(row, column - 1))
+        this.countNumInBasin(row, column - 1, visitedLocations)
+    }
+
+    val numRight = if (visitedLocations.contains(Pair(row, column + 1))) {
+        0
+    } else {
+        visitedLocations.add(Pair(row, column + 1))
+        this.countNumInBasin(row, column + 1, visitedLocations)
+    }
+
+    val result = 1 + numAbove + numBelow + numLeft + numRight
+    println("returning from: matrix[$row][$column] = ${this[row][column]} with $result")
+    return result
 }
